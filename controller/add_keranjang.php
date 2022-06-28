6c392b96f2944c78cc6741047275559887d2121c
yyaayyaatt <yyaayyaatt@gmail.com>
2022-06-28 16:18:05 +0700

    Create add_keranjang.php

---
+++ controller/add_keranjang.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+//include koneksi database
+include('../admin/config/connection.php');
+
+//get data dari form
+$id_produk     = $_GET['id_produk'];
+$pelanggan     = $_SESSION['id_pel'];
+//query insert data ke dalam database
+$query = "INSERT INTO keranjang (id_produk,qty,pelanggan) VALUES ('$id_produk','1','$pelanggan')";
+
+//kondisi pengecekan apakah data berhasil dimasukkan atau tidak
+if ($conn->query($query)) {
+    header("location: ../view/keranjang.php");
+} else {
+
+    //pesan error gagal insert data
+    echo "Data Gagal Disimpan!";
+    header("location: ../view/keranjang.php");
+}
