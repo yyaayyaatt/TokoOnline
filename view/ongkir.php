5a8d4edbb7cbac264b4a3fb8ac9a58810493f642
Nurhidayat <yyaayyaatt@gmail.com>
2022-06-29 23:34:52 +0700

    Update ongkir.php

--- view/ongkir.php
+++ view/ongkir.php
@@ -1,87 +1,137 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
 
-<head>
-    <title>Mari Belajar Coding</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="../css/style.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="../css/font-awesome.min.css" rel="stylesheet">
-    <link href="../css/skdslider.css" rel="stylesheet">
-    <script src="../js/skdslider.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../js/move-top.js"></script>
-    <script type="text/javascript" src="../js/easing.js"></script>
-    <script src="../js/carousel.js"></script>
-    <script src="../admin/dist/js/adminlte.js"></script>
-    <!-- js -->
-    <script src="../admin/plugins/jquery/jquery.min.js"></script>
-    <script src="../admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="js/app.js"></script>
-</head>
+ 
 
-<body>
+ //Get Data Kabupaten 
+  $curl = curl_init();  
+  curl_setopt_array($curl, array( 
+    CURLOPT_URL => "http://api.rajaongkir.com/starter/city", 
+    CURLOPT_RETURNTRANSFER => true, 
+    CURLOPT_ENCODING => "", 
+    CURLOPT_MAXREDIRS => 10, 
+    CURLOPT_TIMEOUT => 30, 
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, 
+    CURLOPT_CUSTOMREQUEST => "GET", 
+    CURLOPT_HTTPHEADER => array( 
+      "Key: Anda bisa isi dengan api key milik Anda sendiri" 
+    ), 
+  )); 
 
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">Mari Belajar Coding</a>
-            </div>
+  $response = curl_exec($curl); 
+  $err = curl_error($curl); 
 
-        </div>
-    </nav>
+  curl_close($curl); 
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-5">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <form class="form-horizontal" id="ongkir" method="POST">
-                            <div class="form-group">
-                                <label class="control-label col-sm-3">Kota Asal:</label>
-                                <div class="col-sm-9">
-                                    <select class="form-control" id="kota_asal" name="kota_asal" required="">
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-sm-3">Kota Tujuan</label>
-                                <div class="col-sm-9">
-                                    <select class="form-control" id="kota_tujuan" name="kota_tujuan" required="">
-                                        <option></option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-sm-3">Kurir</label>
-                                <div class="col-sm-9">
-                                    <select class="form-control" id="kurir" name="kurir" required="">
-                                        <option value="jne">JNE</option>
-                                        <option value="tiki">TIKI</option>
-                                        <option value="pos">POS INDONESIA</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label col-sm-3">Berat (Kg)</label>
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" id="berat" name="berat" required="">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-sm-offset-3 col-sm-8">
-                                    <button type="submit" class="btn btn-default">Cek</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-7" id="response_ongkir">
-            </div>
-        </div>
-    </div>
-</body>
+  echo "<label>Kota Asal</label><br>"; 
+  echo "<select name='asal' id='asal'>"; 
+  echo "<option>Pilih Kota Asal</option>"; 
+  $data = json_decode($response, true); 
+  for ($i=0; $i < count($data['rajaongkir']['results']); $i++) {  
+      echo "<option value='".$data['rajaongkir']['results'][$i]['city_id']."'>".$data['rajaongkir']['results'][$i]['city_name']."</option>"; 
+  } 
+  echo "</select><br><br><br>"; 
+  //Get Data Kabupaten 
 
-</html>
+
+  //----------------------------------------------------------------------------- 
+
+  //Get Data Provinsi 
+  $curl = curl_init(); 
+
+  curl_setopt_array($curl, array( 
+    CURLOPT_URL => "http://api.rajaongkir.com/starter/province", 
+    CURLOPT_RETURNTRANSFER => true, 
+    CURLOPT_ENCODING => "", 
+    CURLOPT_MAXREDIRS => 10, 
+    CURLOPT_TIMEOUT => 30, 
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, 
+    CURLOPT_CUSTOMREQUEST => "GET", 
+    CURLOPT_HTTPHEADER => array( 
+      "Key: c0caca4df9bde60a43d45aeda130b80c" 
+    ), 
+  )); 
+
+  $response = curl_exec($curl); 
+  $err = curl_error($curl); 
+
+  echo "Provinsi Tujuan<br>"; 
+  echo "<select name='provinsi' id='provinsi'>"; 
+  echo "<option>Pilih Provinsi Tujuan</option>"; 
+  $data = json_decode($response, true); 
+  for ($i=0; $i < count($data['rajaongkir']['results']); $i++) { 
+  echo "<option value='".$data['rajaongkir']['results'][$i]['province_id']."'>".$data['rajaongkir']['results'][$i]['province']."</option>"; 
+  } 
+  echo "</select><br><br>"; 
+  //Get Data Provinsi 
+
+ ?> 
+
+ <!DOCTYPE html> 
+ <html> 
+  <head> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+  </head> 
+ 
+  <body> 
+
+  <label>Kabupaten Tujuan</label><br> 
+  <select id="kabupaten" name="kabupaten"></select><br><br> 
+
+  <label>Kurir</label><br> 
+  <select id="kurir" name="kurir"> 
+  <option value="jne">JNE</option> 
+  <option value="tiki">TIKI</option> 
+  </select><br><br> 
+
+  <label>Berat (gram)</label><br> 
+  <input id="berat" type="text" name="berat" value="500" /> 
+  <br><br> 
+
+  <input id="cek" type="submit" value="Cek"/> 
+
+  <div id="ongkir"></div> 
+
+  </body> 
+ </html> 
+
+
+ <script type="text/javascript"> 
+
+  $(document).ready(function(){ 
+  $('#provinsi').change(function(){ 
+
+  //Pada tahap ini value diambil dari option select provinsi kemudian parameternya dikirimkan dengan memakai ajax  
+  var prov = $('#provinsi').val(); 
+
+          $.ajax({ 
+                type : 'GET', 
+               url : 'http://domainAnda.tld/rajaongkir/cek_kabupaten.php', 
+                data :  'prov_id=' + prov, 
+  success: function (data) { 
+
+  //jika data berhasil didapatkan, tampilkan ke dalam option select kabupaten 
+  $("#kabupaten").html(data); 
+  } 
+              }); 
+  }); 
+
+  $("#cek").click(function(){ 
+  //Pada tahap ini value diambil dari option select provinsi asal, kabupaten, kurir, berat kemudian parameternya juga akan dikirimkan pakai ajax  
+  var asal = $('#asal').val(); 
+  var kab = $('#kabupaten').val(); 
+  var kurir = $('#kurir').val(); 
+  var berat = $('#berat').val(); 
+
+          $.ajax({ 
+                type : 'POST', 
+               url : 'http://domainAnda.tld/rajaongkir/cek_ongkir.php', 
+                data :  {'kab_id' : kab, 'kurir' : kurir, 'asal' : asal, 'berat' : berat}, 
+  success: function (data) { 
+
+  //jika data sudah berhasil didapat, akan ditampilkan ke dalam element div ongkir 
+  $("#ongkir").text(data); 
+  } 
+              }); 
+  }); 
+  }); 
+ </script>
