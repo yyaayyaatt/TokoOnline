7b400276841a0831284ec999c2b88f14b50d788e
Nurhidayat <yyaayyaatt@gmail.com>
2022-07-02 23:32:19 +0700

    Update delete_pel.php

--- sadmin/controller/delete_pel.php
+++ sadmin/controller/delete_pel.php
@@ -5,12 +5,13 @@ include('../config/connection.php');
 //get id
 $id_pel = $_GET['id_pel'];
 
-$query = "DELETE FROM pelanggan WHERE id_pel = '$id_pel'";
+$query = "UPDATE pelanggan SET status='0' WHERE id_pel = '$id_pel'";
 
 if($conn->query($query)) {
   header("location: ../view/pelanggan.php");
 } else {
     echo "DATA GAGAL DIHAPUS!";
+    header("location: ../view/pelanggan.php");
 }
 
 ?>
