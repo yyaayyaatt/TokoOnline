58b0823a5c97adea6a569a9d78c6b54caf9c2a3c
Nurhidayat <yyaayyaatt@gmail.com>
2022-07-02 23:32:36 +0700

    Create restore_pel.php

---
+++ sadmin/controller/restore_pel.php
@@ -0,0 +1,17 @@
+<?php
+
+include('../config/connection.php');
+
+//get id
+$id_pel = $_GET['id_pel'];
+
+$query = "UPDATE pelanggan SET status='1' WHERE id_pel = '$id_pel'";
+
+if($conn->query($query)) {
+  header("location: ../view/pelanggan.php");
+} else {
+    echo "DATA GAGAL DIHAPUS!";
+    header("location: ../view/pelanggan.php");
+}
+
+?>
