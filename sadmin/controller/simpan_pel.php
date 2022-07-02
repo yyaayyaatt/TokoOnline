8bd33bb228c8f78f8accc77dc5c25a6ff999c666
Nurhidayat <yyaayyaatt@gmail.com>
2022-07-02 23:32:46 +0700

    Update simpan_pel.php

--- sadmin/controller/simpan_pel.php
+++ sadmin/controller/simpan_pel.php
@@ -8,8 +8,9 @@ $nama           = $_POST['nama'];
 $email           = $_POST['email'];
 $telp           = $_POST['telp'];
 $alamat           = $_POST['alamat'];
+$role           = $_POST['role'];
 //query insert data ke dalam database
-$query = "INSERT INTO pelanggan (nama,email,telp,alamat) VALUES ('$nama','$email','$telp','$alamat')";
+$query = "INSERT INTO pelanggan (nama,email,telp,alamat,role) VALUES ('$nama','$email','$telp','$alamat','$role')";
 
 //kondisi pengecekan apakah data berhasil dimasukkan atau tidak
 if($conn->query($query)) {
@@ -20,7 +21,8 @@ if($conn->query($query)) {
 } else {
 
     //pesan error gagal insert data
-    echo "Data Gagal Disimpan!";
+    Alert ("Data Gagal Disimpan!");
+    header("location: ../view/pelanggan.php");
 
 }
 
